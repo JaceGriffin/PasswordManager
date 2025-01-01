716b6b348491f10ac8dafe8aa7f3bbe7e2963638
JaceGriffin <talakav5205@gmail.com>
2025-01-01 19:24:27 +0200

    feat(stdfiles): func ReadFileToDX

--- main.asm
+++ main.asm
@@ -2,9 +2,7 @@ org 100h
 
 
 call view
- 
 
-end_of_prog:
 
 mov ah, 0
 int 16h
@@ -18,36 +16,13 @@ ret
 
 
 proc view
-    ;open file for reading
-    mov ah, 3Dh
-    mov al, 0
-    mov dx, offset data_filename
-    int 21h
-    
-    ;read from file
-    mov bx, ax
-    mov cx, 100
-    mov dx, offset data_buffer
-    mov ah, 3Fh
-    int 21h
+    push offset data_filename
+    call ReadFileToDX
     
     ;print file content
     mov ah, 9
-    int 21h    
-    ret
-    
-    ;close file
-    mov ah, 3Eh
     int 21h
 endp view
-
-
-proc error
-    mov ah, 9
-    mov dx, offset error_msg
-    int 21h
-    jmp end_of_prog
-endp error
 
 
 
@@ -56,5 +31,10 @@ endp error
 ;---------------------------------------------------------------------------------------------------------------------------------
 
 
+enter_description_msg db "Enter password description: $"
+enter_password_msg db "Enter password: $"
 data_buffer db 100 dup('$')
-data_filename db "..\..\Users\Cyber_User\Desktop\Documents\My_Docs\gitHubRepos\PasswordManager\ata.txt", 0
+data_filename db "..\..\Users\Cyber_User\Desktop\Documents\My_Docs\gitHubRepos\PasswordManager\data.txt", 0
+
+
+include stdfiles.inc
