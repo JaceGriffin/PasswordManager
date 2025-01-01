4f8a29aa4583ea614bd44d8669549b4c2ff9f365
JaceGriffin <talakav5205@gmail.com>
2025-01-01 16:12:32 +0200

    feat(view): print file content

--- main.asm
+++ main.asm
@@ -1,8 +1,7 @@
 org 100h
 
 
-nop
-nop
+call view
 
 
 mov ah, 0
@@ -16,10 +15,26 @@ ret
 ;---------------------------------------------------------------------------------------------------------------------------------
 
 
-proc f
+proc view
+    ;open file for reading
+    mov ah, 3Dh
+    mov al, 0
+    mov dx, offset data_filename
+    int 21h
     
+    ;TD: handle file not found
+    ;read from file
+    mov bx, ax
+    mov cx, 100
+    mov dx, offset data_buffer
+    mov ah, 3Fh
+    int 21h
+    
+    ;print file content
+    mov ah, 9
+    int 21h    
     ret
-endp f   
+endp view   
 
 
 
@@ -28,4 +43,5 @@ endp f
 ;---------------------------------------------------------------------------------------------------------------------------------
 
 
-var dw 0
+data_buffer db 100 dup('$')
+data_filename db "..\..\Users\Cyber_User\Desktop\Documents\My_Docs\gitHubRepos\PasswordManager\data.txt", 0
