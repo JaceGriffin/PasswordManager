33f8c7cdbf1443d62764d349277eab9f15f0233d
Jace-Griffin <talakav139@gmail.com>
2025-01-02 19:16:53 +0200

    feat(stdfiles): WriteDxToFile

--- main.asm
+++ main.asm
@@ -1,7 +1,7 @@
 org 100h
 
 
-call view
+call add
 
 
 mov ah, 0
@@ -16,13 +16,37 @@ ret
 
 
 proc view
+    ;read file
     push offset data_filename
+    mov dx, offset data_buffer
     call ReadFileToDX
     
     ;print file content
     mov ah, 9
     int 21h
+    
+    ret
 endp view
+
+
+proc add
+    ;get user input
+    mov dx, offset enter_description_msg
+    mov ah, 9
+    int 21h
+    mov dx, offset data_buffer
+    mov ah, 0ah
+    int 21h
+    
+    ;write input to file
+    mov dx, offset data_buffer+2
+    push offset data_filename
+    call WriteDxToFile
+    
+    ret
+endp add
+    
+    
 
 
 
@@ -33,7 +57,7 @@ endp view
 
 enter_description_msg db "Enter password description: $"
 enter_password_msg db "Enter password: $"
-data_buffer db 100 dup('$')
+data_buffer db 100h dup('0')
 data_filename db "..\..\Users\Cyber_User\Desktop\Documents\My_Docs\gitHubRepos\PasswordManager\data.txt", 0
 
 
